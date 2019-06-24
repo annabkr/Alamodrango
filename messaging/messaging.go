6286177f48c9a848a37f7a379e97c19c2126d90f
Anna Baker <44475953+annabkr@users.noreply.github.com>
2019-06-24 09:54:36 -0700

    Update messaging.go

--- messaging/messaging.go
+++ messaging/messaging.go
@@ -30,11 +30,11 @@ func RunMessenger() {
 
 
 func NewTextMessage() TextMessage {
-  sid := "ACd93ff7e1ea248a23e77e273f0e0a9315"
-  auth := "c2659fe77318e206b34fa5485d1dc062"
+  sid := "DISABLEDFORDEMO"
+  auth := "DISABLEDFORDEMO"
   url := "https://api.twilio.com/2010-04-01/Accounts/" + sid + "/Messages.json"
-  toField := "14158280722"
-  fromField := "14157669491"
+  toField := "DISABLED"
+  fromField := "DISABLEDFORDEMO"
   quote := "Hi, Anna! It looks like Alamo Drafthouse has updated their movie showtimes for this weekend."
 
   return TextMessage{To: toField, From: fromField, AccountSid: sid, AuthToken: auth, UrlStr: url, Text: quote}
